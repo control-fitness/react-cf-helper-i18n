b93df61bc71098f8ce3ea06ad696d0270bd9dc63
Paulo McNally <paulomcnally@gmail.com>
2018-07-16 20:51:27 -0600

    v0.0.3

--- src/helpers/locales/english.js
+++ src/helpers/locales/english.js
@@ -3,10 +3,25 @@ module.exports = {
     create: {
       header: 'Create an account',
       company: {
-        header: 'Fitness center data'
+        header: 'Fitness center data',
+        form: {
+          name: 'Name',
+          tenant: 'Domain',
+          country: 'Country',
+          currency: 'Currency',
+          tax: 'Tax',
+          taxPercentage: 'Tax percentage',
+          timeZone: 'Time zone',
+          locale: 'Language'
+        }
       },
       user: {
-        header: `Owner's basic information`
+        header: `Owner's basic information`,
+        form: {
+          name: 'Name',
+          email: 'Email',
+          password: 'Password'
+        }
       }
     }
   }
--- src/helpers/locales/spanish.js
+++ src/helpers/locales/spanish.js
@@ -3,10 +3,25 @@ module.exports = {
     create: {
       header: 'Abre una cuenta',
       company: {
-        header: 'Datos del centro'
+        header: 'Datos del centro',
+        form: {
+          name: 'Nombre',
+          tenant: 'Dominio',
+          country: 'País',
+          currency: 'Moneda',
+          tax: 'Impuesto',
+          taxPercentage: 'Porcentaje de impuesto',
+          timeZone: 'Zona horaria',
+          locale: 'Idioma'
+        }
       },
       user: {
-        header: 'Datos del propietario'
+        header: 'Datos del propietario',
+        form: {
+          name: 'Nombre',
+          email: 'Correo electrónico',
+          password: 'Contraseña'
+        }
       }
     }
   }
